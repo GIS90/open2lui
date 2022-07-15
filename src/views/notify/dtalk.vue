dfd6e2709b08b082abd69f357b99693c42531252
mingliang.gao <gaoming971366@163.com>
2022-07-15 09:58:32 +0800

    dtalk页码开发

--- src/views/notify/dtalk.vue
+++ src/views/notify/dtalk.vue
@@ -1,10 +1,101 @@
 <template>
-  <div class="app-container">钉钉绩效</div>
+  <div class="app-container">
+    <!-- 按钮 -->
+    <el-row>
+      <el-button id="btn-upload" :size="btnBaseAttrs.size" :plain="btnBaseAttrs.plain" :round="btnBaseAttrs.round" :disabled="btnDisabled" @click="openFileUpload">
+        <svg-icon icon-class="i_upload" />  上传
+      </el-button>
+      <el-button id="btn-select" class="btn-margin" :plain="btnBaseAttrs.plain" :round="btnBaseAttrs.round" :size="btnBaseAttrs.size" :disabled="btnDisabled" @click="manualSelectALL">
+        <svg-icon icon-class="i_select" />  {{ selBtnText }}
+      </el-button>
+      <el-button id="btn-delete" class="btn-margin" :plain="btnBaseAttrs.plain" :round="btnBaseAttrs.round" :size="btnBaseAttrs.size" :disabled="btnDisabled" @click="openDeleteDialog">
+        <svg-icon icon-class="i_delete" />  删除
+      </el-button>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import Pagination from '@/components/Pagination'
+
 export default {
-  name: 'Dtalk'
+  name: 'Dtalk',
+  emits: [],
+  components: {
+    'pagination': Pagination
+  },
+  props: {},
+  data() {
+    return {
+      fileType: '5', // 文件类型：1:word 2:excel 3:ppt 4:文本 5:pdf 6:其他
+      selBtnText: '全选', // 选择按钮内容
+      btnUploadLoading: false, // 上传按钮加载中状态
+      btnMergeLoading: false, // 合并按钮加载中状态
+      btnDeleteLoading: false, // 删除按钮加载中状态
+      btnDisabled: false, // 按钮禁用状态
+      // button attributes
+      btnBaseAttrs: {
+        size: 'medium', // 大小 medium / small / mini / ''
+        type: 'primary', // 类型 primary / success / warning / danger / info / text
+        plain: true, // 是否为朴素按钮
+        round: false, // 是否为圆角按钮
+        circle: false // 是否为圆形按钮
+      },
+      uploadDialogStatus: false, // 文件上传dialog
+      // table attributes
+      tableAttrs: {
+        stripe: true, // 是否为斑马纹 true/false
+        border: true, // 是否带有纵向边框 true/false
+        size: 'medium', // 尺寸 medium / small / mini / ''
+        fit: true, // 列的宽度是否自撑开 true/false
+        showHeader: true, // 是否显示表头 true/false
+        hcr: true, // 是否要高亮当前行highlight-current-row true/false
+        height: 450, // 高度
+        maxHeight: 450, // 最大高度(属性为Table指定最大高度，此时若表格所需的高度大于最大高度，则会显示一个滚动条。)
+        showSum: true, // 表尾合计
+        sumText: '统计', // 合计行第一列的文
+        emptyText: '暂无数据' // 空数据时显示的文本内容
+      },
+      // table row: label column-key prop width min-width sortable
+      tableRowAttrs: {
+        type: 'selection', // selection多选框 / index索引 / expand可展开的按钮
+        index: 1, // 如果设置了 type=index，可以通过传递 index 属性来自定义索引
+        fixed: true, // 列固定，值有left、right，默认为left
+        resizable: true, // 对应列是否可以通过拖动改变宽度 true/false
+        align: 'center', // 对齐方式 left / center / right
+        headerAlign: 'center', // 表头对齐方式，若不设置该项，则使用表格的对齐方式 left / center / right
+        sot: true // showOverflowTooltip 多余的内容会在hover时以tooltip的形式显示出来
+      },
+      // pagination attrs
+      pageCur: 1, // 当前page
+      pageSize: 15, // 每页显示条目个数
+      pageTotal: 0, // 总条数
+      selectAllStatus: false, // 全选状态
+      selectList: [], // 选择列表
+      tableData: [], // table data
+      oprSelectRowMd5: '', // 当前选择data rtx
+      setDialogStatus: false, // 设置dialog状态
+      toDialogStatus: false, // PDF转WORD-dialog状态
+      mergeDialogStatus: false, // 合并dialog状态
+      deleteSource: 'office-pdf', // delete source
+      deleteConfirm: false // 删除确认dialog状态
+    }
+  },
+  computed: {},
+  watch: {
+    selectAllStatus(newVal, oldVal) { // watch select button status
+      if (newVal) {
+        this.selBtnText = '取消'
+      } else {
+        this.selBtnText = '全选'
+      }
+    }
+  },
+  created() {
+    this.getTableList()
+  },
+  mounted() {},
+  methods: {}
 }
 </script>
 
