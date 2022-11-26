ee952df8ec97c95e5d1860f7d77f7e9aa15469de
mingliang.gao <gaoming971366@163.com>
2022-11-26 11:00:03 +0800

    dtalk dialog添加全屏功能

--- src/services/notify/DtalkSend.vue
+++ src/services/notify/DtalkSend.vue
@@ -2,9 +2,8 @@
   <div>
     <el-dialog
       :visible="show"
-      :title="dialogAttrs.title"
       :width="dialogAttrs.width"
-      :fullscreen="dialogAttrs.fullScreen"
+      :fullscreen="fullScreenStatus"
       :top="dialogAttrs.top"
       :modal="dialogAttrs.modal"
       :lock-scroll="dialogAttrs.lockScroll"
@@ -18,6 +17,21 @@
       @open="openDialog()"
       @close="closeDialog()"
     >
+      <!--title-->
+      <template #title>
+        <div @dblclick="handleFull">
+          <span class="dialog-title">
+            <span v-text="dialogAttrs.title" />
+            <el-tooltip class="item" effect="dark" content="关闭" placement="top">
+              <i class="el-icon-close dialog-title-close" @click="closeDialog" />
+            </el-tooltip>
+            <el-tooltip class="item" effect="dark" :content="fullScreenText" placement="top">
+              <i :class="[fullScreenIcon, 'dialog-title-full']" @click="handleFull" />
+            </el-tooltip>
+          </span>
+        </div>
+      </template>
+      <!--content-->
       <el-form id="main-opr-div" ref="formData" :label-position="labelPosition" :model="formData" label-width="auto" style="width: 100%">
         <el-form-item label="Robot选择">
           <el-select
@@ -104,6 +118,9 @@ export default {
     return {
       disabled: false, // 禁用组件
       labelPosition: 'left', // label-position 属性可以改变表单域标签的位置，可选值为 top、left、right
+      fullScreenStatus: false, // DIALOG是否全屏状态，默认false
+      fullScreenIcon: 'el-icon-full-screen', // DIALOG全屏图标
+      fullScreenText: '全屏', // DIALOG全屏文本提示
       dialogAttrs: {
         title: '消息发送',
         width: '45%', // Dialog 的宽度
@@ -115,7 +132,7 @@ export default {
         closeDelay: 0, // Dialog 关闭的延时时间，单位毫秒
         closeOnClickModal: true, // 是否可以通过点击 modal 关闭 Dialog
         closeOnPressEscape: true, // 是否可以通过按下 ESC 关闭 Dialog
-        showClose: true, // 是否显示关闭按钮
+        showClose: false, // 是否显示关闭按钮
         draggable: false, // 为 Dialog 启用可拖拽功能
         center: false // 是否让 Dialog 的 header 和 footer 部分居中排列
       },
@@ -138,7 +155,12 @@ export default {
     }
   },
   computed: {},
-  watch: {},
+  watch: {
+    fullScreenStatus(newVal, oldVal) {
+      newVal ? this.fullScreenIcon = 'el-icon-copy-document' : this.fullScreenIcon = 'el-icon-full-screen'
+      newVal ? this.fullScreenText = '缩小' : this.fullScreenText = '全屏'
+    }
+  },
   created() {},
   mounted() {},
   methods: {
@@ -147,10 +169,21 @@ export default {
         this.$emit('close-send-dg', true)
         return false
       }
-      this.getInitData()
+      // 初始化非全屏
+      this.fullScreenStatus = false
+      this.$nextTick(() => {
+        this.getInitData()
+        // 重置表单状态
+        // this.$refs.formData.resetFields()
+      })
     },
     closeDialog() { // 关闭dg
+      // 清空表单状态
+      // this.$refs.formData.clearValidate()
       this.$emit('close-send-dg', false)
+    },
+    handleFull() { // 是否全屏model
+      this.fullScreenStatus = !this.fullScreenStatus
     },
     getInitData() {
       const data = {
