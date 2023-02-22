41380dcfb9b37d1cfea5dad5bfaeeada322c51c6
mingliang.gao <gaoming971366@163.com>
2023-02-22 20:16:30 +0800

    调整request.js>axios>timeout参数

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import { getToken } from '@/utils/auth'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 600 * 1000, // request timeout 600s = 10min
+  timeout: 1000 * 60 * 5, // request timeout 600s = 10min
   withCredentials: true // CORS credential
 })
 
